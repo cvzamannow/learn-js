fe910705c2bbdab2b072f15da79898047818094c
Nurrohmann <nurrohmanmarduki@gmail.com>
2023-12-19 10:32:04 +0700

    changes

---
+++ function/function.js
@@ -0,0 +1,22 @@
+//Pendefinisian Fungsi
+function greeting() {
+  console.log("Selamat dataang");
+}
+
+//Pemanggilan Fungsi
+greeting();
+
+//Parameter dan Argumen
+function myGreeting(param) {
+  console.log(param);
+}
+
+myGreeting("Selamat datang di CVZN Teams");
+myGreeting("Selamat datang di Back End Teams");
+
+//Nilai Kembalian
+function myInfo(bio) {
+  return bio;
+}
+
+console.log(myInfo("Halo namaku Nurrohman"));
---
+++ function/study-case.js
@@ -0,0 +1,14 @@
+function greetingPropgram(name) {
+  return "halo selamat datang," + name + ", di CV ZAMAN NOW";
+}
+
+console.log(greetingPropgram("Rohman"));
+
+//buat function untuk menambah bilangan
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(add(10, 5));
+
+//buat function pengurangan, perkalian, sama pembagian
